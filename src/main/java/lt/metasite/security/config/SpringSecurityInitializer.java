d92703b2f6dee7349c41a3c2d05d784a29060b90
juozasa <juozas.adamonis@metasite.lt>
2020-03-14 15:25:26 +0200

    init

---
+++ src/main/java/lt/metasite/security/config/SpringSecurityInitializer.java
@@ -0,0 +1,8 @@
+package lt.metasite.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+//code is not needed
+}
